bfcf343340da08e1b258fca6ddaa95d859e3dce4
jpbscolab <150890211+jpbscolab@users.noreply.github.com>
2025-09-04 19:40:08 +0900

    Initial commit

---
+++ src/project_name/__init__.py
@@ -0,0 +1,16 @@
+"""Project name package."""
+
+# TODO: 実際のプロジェクトの実装に合わせて更新してください
+# from .core.example import ExampleClass, ExampleConfig, process_data
+# from .utils.logging_config import get_logger, set_log_level, setup_logging
+
+__all__ = [
+    # "ExampleClass",
+    # "ExampleConfig",
+    # "get_logger",
+    # "process_data",
+    # "set_log_level",
+    # "setup_logging",
+]
+
+__version__ = "0.1.0"
